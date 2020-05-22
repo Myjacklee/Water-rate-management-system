31325f5d0683d24dc133f75c63c446507acad73a
Myjacklee <2251222797@qq.com>
2020-05-22 14:37:37 +0800

    add the website

--- logout.php
+++ logout.php
@@ -1,7 +1,6 @@
 <?php
 session_start();
 if(isset($_SESSION["loginStatus"]["status"])&&$_SESSION["loginStatus"]["status"]===true){
-    session_unset();
     session_destroy();
     setcookie(session_name(),'',time()-3600);
     echo true;
