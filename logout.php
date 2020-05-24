bfc37dfbdec5b1cd81fb597affac20d54df456de
Myjacklee <2251222797@qq.com>
2020-05-24 19:47:43 +0800

    超级管理员注销登录模块

--- logout.php
+++ logout.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if(isset($_SESSION["loginStatus"]["status"])&&$_SESSION["loginStatus"]["status"]===true){
+if((isset($_SESSION["loginStatus"]["status"])&&$_SESSION["loginStatus"]["status"]==true)||(isset($_SESSION["admin"])&&$_SESSION["admin"]==true)){
     $_SESSION=array();
     if(isset($_COOKIE[session_name()])){
         setcookie(session_name(),'',time()-42000,'/');
